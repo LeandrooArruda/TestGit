78fe15c233a0aa9ec62afac818be76291d0835f7
Leandro <leandropda96@gmail.com>
2020-10-05 12:25:26 -0300

    Sec Commit

--- Course/Program.cs
+++ Course/Program.cs
@@ -19,16 +19,9 @@ namespace Course
             double preco2 = 650.50;
             double medida = 53.234567;
 
-            Console.WriteLine("Produtos: ");
-            Console.WriteLine(produto1 + "," + " cujo preço é $" + preco1);
-            Console.WriteLine(produto2 + "," + " cujo preço é $" + preco2.ToString("F2"));
-            Console.WriteLine( );
-            Console.WriteLine("Registro: ");
-            Console.WriteLine(idade + " anos de idade, código " + codigo + " e gênero: " + genero);
-            Console.WriteLine();
-            Console.WriteLine("Medida com oito casas decimais: " + medida.ToString("F8"));
-            Console.WriteLine("Arredondado (três casas decimais): " + medida.ToString("F3"));
-            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));   
+
+            Console.WriteLine($"Idade {idade}, Tipo {produto1}, Cod {codigo}");
+             
 
 
         }
